471e7135c27fb3b178de409da6b5d195f5f0f539
aluno24257 <105852495+aluno24257@users.noreply.github.com>
2023-04-07 20:28:53 +0100

    Initial commit

---
+++ enviar-mensagem.php
@@ -0,0 +1,47 @@
+<?php
+if(isset($_POST['email'])) {
+
+$email_to = "brunocarreira2003@hotmail.com";
+$email_subject = "Contato pelo Site";
+
+function died($error) {
+    echo "Lamentamos, mas ocorreram erros no formulário enviado. ";
+    echo "Esses erros aparecem abaixo.<br /><br />";
+    echo $error."<br /><br />";
+    echo "Por favor, volte e corrija esses erros.<br /><br />";
+    die();
+}
+
+if(!isset($_POST['nome']) ||
+    !isset($_POST['email']) ||
+    !isset($_POST['mensagem'])) {
+    died('Lamentamos, mas parece haver um problema com os dados enviados.');       
+}
+
+$nome = $_POST['nome']; 
+$email = $_POST['email']; 
+$mensagem = $_POST['mensagem']; 
+
+$email_message = "Detalhes do formulário de contato abaixo.\n\n";
+
+function clean_string($string) {
+  $bad = array("content-type","bcc:","to:","cc:","href");
+  return str_replace($bad,"",$string);
+}
+
+$email_message .= "Nome: ".clean_string($nome)."\n";
+$email_message .= "Email: ".clean_string($email)."\n";
+$email_message .= "Mensagem: ".clean_string($mensagem)."\n";
+
+$headers = 'From: '.$email."\r\n".
+'Reply-To: '.$email."\r\n" .
+'X-Mailer: PHP/' . phpversion();
+@mail($email_to, $email_subject, $email_message, $headers); 
+?>
+
+<!-- Mensagem de sucesso -->
+Obrigado por entrar em contato conosco. Responderemos assim que possível.
+
+<?php
+}
+?>
